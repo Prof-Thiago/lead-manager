238e1014a555be0118bb079bd59520d3768b14b5
Professor Thiago <professorthiagohenriqueds@gmail.com>
2023-11-06 06:33:42 -0300

    fix: Apply useState, with isAuthenticated and setIsAuthenticated, and useEffect to fix bug when entering in a page with a user saved in localStorage where the header sign up/logout was not updated correctly.

--- components/styles/header/index.tsx
+++ components/styles/header/index.tsx
@@ -1,6 +1,6 @@
 import { useUser } from "@/providers/userProvider";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 
@@ -8,10 +8,16 @@ export default function MainHeader(props: any) {
     const { user, handleLogout } = useUser();
     const route = useRouter();
 
+    const [isAuthenticated, setIsAuthenticated] = useState(!!user.id || false)
+
     const logout = () => {
         handleLogout();
         route.push('/')
     }
+
+    useEffect(() => {
+        setIsAuthenticated(!!user.id || false)
+    })
 
     return (
         <header className="bg-white w-full h-24 absolute">
@@ -23,7 +29,7 @@ export default function MainHeader(props: any) {
                 
                 <div className="lg:flex lg:flex-1 lg:justify-end">
                     <p className="text-sm font-semibold leading-6 text-gray-900" onClick={logout}>
-                        {!!user.id ? 'Log out' : 'Sign up'}
+                        { isAuthenticated ? 'Log out' : 'Sign Up' }
                     </p>
                 </div>
 
