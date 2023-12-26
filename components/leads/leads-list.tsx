0f4112d7652084e49678a5556160121f3ede52a2
Professor Thiago <professorthiagohenriqueds@gmail.com>
2023-12-26 20:59:16 -0300

    refactor: Change logic and incorporate filter in LeadList useEffect to properly work with lead and user providers.

--- components/leads/leads-list.tsx
+++ components/leads/leads-list.tsx
@@ -1,19 +1,34 @@
 import { useUser } from "@/providers/userProvider";
 import LeadItem from "./lead-item";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Droppable, Draggable } from "react-beautiful-dnd";
+import { useLead } from "@/providers/leadProvider";
+import { Lead } from "@/utils/interfaces";
 
 export default function LeadList(props: any) {
     const { setModal, listStatus, children } = props;
-    const { userLeads, getUserLeads } = useUser();
+    const { user } = useUser();
+    const { leads } = useLead();
+    const [ userLeads, setUserLeads ] = useState<Lead[]>([])
 
     useEffect(() => {
-        getUserLeads();
-    }, [])
+        const filteredLeads = leads.filter(lead => lead.userId === user.id);
+
+        setUserLeads(filteredLeads);
+    }, [leads])
 
     return (
-        <div className="border-solid border-gray-200 border-2 w-1/3 m-0 p-0">
-            <div className="text-center flex flex-col justify-center bg-black text-center h-20">
+    <Droppable 
+        droppableId={listStatus} 
+        key={listStatus}
+        direction="vertical">
+        {(provided) =>
+        <div 
+            className="w-60 m-1"                         
+            {...provided.droppableProps} 
+            ref={provided.innerRef}>
+
+            <div className="text-center flex flex-col justify-center bg-black text-center h-20 rounded-t-xl">
                 <h3 className="text-white text-base">
                     { children }
                 </h3>                
@@ -21,49 +36,45 @@ export default function LeadList(props: any) {
 
  
 
-            <Droppable 
-                droppableId={listStatus} 
-                key={listStatus}
-                direction="vertical">
-                {(provided) =>
-                    <ul 
-                        role="list" 
-                        className="max-h-md divide-gray-100 divide-y-2"
-                        {...provided.droppableProps} 
-                        ref={provided.innerRef}
-                        >
-                    {userLeads && userLeads.map((lead, index) =>  (
-                        <li key={index}>
-                                <Draggable 
-                                    key={lead.id + listStatus} 
-                                    draggableId={lead.id + listStatus} 
-                                    index={index}
-                                    >
-                                    {(prov) => {
-                                        return (
-                                            <div 
-                                                ref={prov.innerRef} 
-                                                {...prov.draggableProps} 
-                                                {...prov.dragHandleProps}
-                                                className="p-2" 
-                                                >
 
-                                                <LeadItem 
-                                                    lead={lead} 
-                                                    setModal={setModal}
-                                                    showStatus={listStatus}
-                                                    />
-                                                                                            
-                                            </div>                                        
-                                    )}}
-                                </Draggable>
+            <ul 
+                role="list" 
+                className="border-solid border-gray-200 border-2 rounded-b-xl"
+                >
+                {userLeads && userLeads.map((lead, index) =>  { 
+                    if (lead.status === listStatus ) { return (
+                        <Draggable 
+                            key={lead.id + listStatus} 
+                            draggableId={lead.id + listStatus} 
+                            index={index}
+                            >
+                            {(prov) => {
+                                return (
+                            <li key={index} 
+                                ref={prov.innerRef} 
+                                {...prov.draggableProps} 
+                                {...prov.dragHandleProps}
+                                className="h-36 p-2 text-center flex flex-col justify-center"
+                                >
+                                <div>
+                                    <div>
+                                        <LeadItem 
+                                            lead={lead} 
+                                            setModal={setModal}
+                                            listStatus={listStatus}
+                                            />                                                     
+                                    </div>                                        
+                                </div>
                             </li>
-                            )                                
-                        )}
-                        {provided.placeholder}
-                    </ul>  
-                }
-            </Droppable>                                              
+                        )}}
+                        </Draggable> 
+                    )}
+                })}
+                { provided.placeholder }
+                <li className="h-5 bg-gray-100"></li>
+            </ul>                                    
         </div>
+        }
+    </Droppable>  
     )
 }
