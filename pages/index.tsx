311949968607cd57c84fa7e0a46c366b85ebb490
Professor Thiago <professorthiagohenriqueds@gmail.com>
2023-12-26 17:56:17 -0300

    feature: Create new homepage for app and change sign up page into new route.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { useUser } from "../providers/userProvider";
-import SignUpForm from '../components/account/signup-form';
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import PageWrapper from "@/components/styles/page-wrapper";
 
@@ -14,19 +13,38 @@ export default function Home() {
     }
   }, [])
 
+  const goToLoginPage = () => {
+    route.push('/login');
+  }
+
+  const goToSignUpPage = () => {
+    route.push('/sign-up');
+  }
+
   return (
     <PageWrapper>
-      <div className="flex min-h-full flex-col justify-between px-6 py-12 lg:px-8 bg-white rounded-md">
-        <h2 className="text-2xl text-center font-bold tracking-tight text-black mb-5">Criar Conta</h2>
+      <div className="flex min-h-64 flex-col justify-between px-6 py-12 lg:px-8 bg-white rounded-md">
+        <h2 className="text-2xl text-center font-bold tracking-tight text-indigo-500 mb-5">Lead Manager</h2>
 
-        <div className="mb-5">
-          <SignUpForm />
+        <div>
+          <p className="mt-5 text-center text-sm text-gray-500 mb-5">
+            Não tem conta?            
+          </p>
+          
+          <button onClick={goToSignUpPage} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >
+              Criar Conta
+          </button>
         </div>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Já tem conta? 
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Entre na sua conta.</a>
-        </p>
+        <div>
+          <p className="mt-5 text-center text-sm text-gray-500 mb-5">
+            Já tem conta?            
+          </p>
+
+          <button onClick={goToLoginPage} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Entrar
+          </button>
+        </div>
       </div>      
     </PageWrapper>
   )
---
+++ pages/sign-up/index.tsx
@@ -0,0 +1,33 @@
+import { useUser } from "../../providers/userProvider";
+import SignUpForm from '../../components/account/signup-form';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import PageWrapper from "@/components/styles/page-wrapper";
+
+export default function Home() {
+  const { user } = useUser();
+  const route = useRouter();
+
+  useEffect( () => {
+    if (!!user.id) {
+      route.push('/leads');
+    }
+  }, [])
+
+  return (
+    <PageWrapper>
+      <div className="flex min-h-64 flex-col justify-between px-6 py-12 lg:px-8 bg-white rounded-md">
+        <h2 className="text-2xl text-center font-bold tracking-tight text-black mb-5">Criar Conta</h2>
+
+        <div className="mb-5">
+          <SignUpForm />
+        </div>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Já tem conta? 
+          <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Entre na sua conta.</a>
+        </p>
+      </div>      
+    </PageWrapper>
+  )
+}
