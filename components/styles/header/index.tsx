07c1a9bea08dd865672c80818d9fe2d8158ae610
Professor Thiago <professorthiagohenriqueds@gmail.com>
2023-12-26 18:00:14 -0300

    refactor: Remade logout function to properly work with new lead and user providers. Change into navbar to implement option for login when user is not logged.

--- components/styles/header/index.tsx
+++ components/styles/header/index.tsx
@@ -1,28 +1,49 @@
+import { useLead } from "@/providers/leadProvider";
 import { useUser } from "@/providers/userProvider";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { opportunitiesBase } from "@/utils/constants";
 
 
 
 export default function MainHeader(props: any) {
-    const { user, handleLogout } = useUser();
+    const { user, setUser } = useUser();
+    const { lead, setLead } = useLead();
     const route = useRouter();
 
     const [winReady, setwinReady] = useState(false);
 
     const logout = () => {
-        handleLogout();
-        route.push('/')
+        localStorage.removeItem('@LeadManager:User')
+        setUser({
+            id: "",
+            name: "",
+            email: "",
+            password: ""
+        });
+        
+        if (!!lead.id) {
+            localStorage.removeItem('@LeadManager:Lead')
+            setLead({
+                id: "",
+                userId: "",
+                status: "potential-client",
+                name: "",
+                email: "",
+                phone: "",
+                opportunities: opportunitiesBase
+            });            
+        }
+        route.push('/');
     }
-
 
     useEffect( () => {
         setwinReady(true);
     }, [])
 
     return (
-        <header className="bg-white w-full h-24 absolute">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <header className="bg-white w-full h-16 absolute">
+            <nav className="mx-auto flex items-center justify-between p-2 lg:px-8" aria-label="Global">
 
                 <div className="flex lg:flex-1">
                     <h1 className="text-3xl text-center font-bold tracking-tight text-black">Lead Manager</h1>
@@ -30,9 +51,19 @@ export default function MainHeader(props: any) {
                 
                 <div className="lg:flex lg:flex-1 lg:justify-end">
                     {winReady 
-                        ?   <a href="/" className="text-sm font-semibold leading-6 text-gray-900" onClick={logout}>
-                                { !!user.id ? 'Log out' : 'Sign Up' }
-                            </a>
+                        ?   !!user.id ?
+                                <a href="/" className="text-sm font-semibold leading-6 text-gray-900" onClick={logout}>
+                                    Sair
+                                </a>
+                                : <span>
+                                    <a href="/login" className="mr-2 text-sm font-semibold leading-6 text-gray-900 hover:text-indigo-400" onClick={logout}>
+                                        Entrar
+                                    </a>
+                                    |
+                                    <a href="/sign-up" className="ml-2 text-sm font-semibold leading-6 text-gray-900 hover:text-indigo-400" onClick={logout}>
+                                        Criar Conta
+                                    </a>
+                                </span>
                         :   <a></a>
                     }
 
