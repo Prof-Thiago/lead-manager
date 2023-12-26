fb0a780d4937195e726528b11dc07eaa23114a9b
Professor Thiago <professorthiagohenriqueds@gmail.com>
2023-12-26 18:07:49 -0300

    style: Implement logic into LeadItem component to change color accordingly with status. Modify the PageWrapper component to properly position elements.

--- components/leads/lead-item.tsx
+++ components/leads/lead-item.tsx
@@ -3,8 +3,18 @@ import { saveInLocalStorage } from '@/utils/localStorage';
 
 export default function LeadItem(props: any) {
 
-    const { lead, showStatus, setModal } = props;
+    const { lead, setModal, listStatus } = props;
     const { setLead } = useLead();
+
+    const getItemColor = ( status: string ) => {
+        switch (status) {
+            case 'potential-client': return 'bg-green-200';
+            case 'confirmed-data': return 'bg-teal-200';
+            case 'lead-review': return 'bg-blue-200';
+            default: return 'bg-gray-200'
+        }
+    
+    }
 
     const setUpdateForm = () => {
         saveInLocalStorage('@LeadManager:Lead', lead)
@@ -13,17 +23,12 @@ export default function LeadItem(props: any) {
     }
 
     return (
-        <div className="w-48 p-2 overflow-hidden">
-            { (lead && lead.id && lead.status === showStatus) ?
-                    ( 
-                        <div className="h-24 bg-gray-200 p-2 text-center flex flex-col justify-center">
-                            <a onClick={setUpdateForm} className="text-sm font-semibold leading-6 text-gray-900 cursor-pointer">
-                                { lead.name }
-                            </a>
-                        </div>
-                    )
-                : (<div className="h-24"></div>) 
-            }
-        </div>
+    <div 
+        className={`w-48 p-2 overflow-hidden h-24 p-2 text-center flex flex-col justify-center rounded-2xl ${ getItemColor(listStatus) }`}
+        >
+        <a onClick={setUpdateForm} className="text-sm font-semibold leading-6 text-gray-900 cursor-pointer">
+            { lead.name }
+        </a>
+    </div>
     )
 }
--- components/styles/page-wrapper/index.tsx
+++ components/styles/page-wrapper/index.tsx
@@ -1,6 +1,6 @@
 export default function PageWrapper(props: any) {
     return (
-        <div className="flex items-center min-w-full box-border min-h-full flex-col justify-between pt-32 pb-10 pr-10 pl-10">
+        <div className="flex items-center min-w-full box-border min-h-full flex-col justify-center pt-32 pb-10 pr-10 pl-10">
             { props.children }   
         </div>
     )
